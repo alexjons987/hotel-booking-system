fca2f77753e28f44f819d3235f1905857c2f9012
alexjons987 <alexander.jonsson987@outlook.com>
2025-10-29 13:10:31 +0100

    Add mysql driver, Add "add customer" functionality

--- src/services/CustomerService.java
+++ src/services/CustomerService.java
@@ -1,5 +1,12 @@
 package services;
 
-public class CustomerService {
+import dao.impl.CustomerDAOImpl;
+import models.Customer;
 
+public class CustomerService {
+    CustomerDAOImpl customerDAO = new CustomerDAOImpl();
+
+    public void addCustomer(Customer customer) {
+        customerDAO.addCustomer(customer);
+    }
 }
